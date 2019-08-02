630677ae0d3aff1e48f50cb2a7df1d6ae52e19ad
Claude Bajada <c.bajada@fz-juelich.de>
2019-08-02 13:37:54 +0200

    improved documentation

--- synthetic_mprage.m
+++ synthetic_mprage.m
@@ -5,7 +5,7 @@ function [T1w_image , T1w_header] = synthetic_mprage(R1, PD, R2_star)
 % The synthetic T1w parameters were obtained from this paper. These were in
 % turn obtained from Tardif et al. (2009).
 % 
-% T1w = synthetic_mprage(R1)
+% [T1w_image , T1w_header] = synthetic_mprage(R1, PD, R2_star)
 % 
 
 % probably should insert some check to see if R1, PD and R2_star seem to be
@@ -13,37 +13,56 @@ function [T1w_image , T1w_header] = synthetic_mprage(R1, PD, R2_star)
 % 
 % would be nice if i could give the option to insert own parameters.
 
+%% read in the header
 header = spm_vol(R1);
-R1_image = spm_read_vols(header);
+
+%% read in the R1 PD and R2_star maps (assumes that they are in seconds)
+R1 = spm_read_vols(header);
+R1(R1<0)=0;% remove negatives
 
 PD = spm_read_vols(spm_vol(PD));
+PD(PD<0) = 0; % remove negatives
+
 R2_star = spm_read_vols(spm_vol(R2_star));
+R2_star(R2_star<0) = 0; % remove negatives
 
-flip_angle = 9; % in degrees
-echo_spacing = 0.0099; % in seconds (this is because R1, PD and R2_star are in seconds in our data)
-TI = .960; % in seconds
-readout_duration = 176 * echo_spacing; % in seconds
-TR = 2.42;
-delay_time = TR - (TI + (readout_duration/2));
+%% Set parameters
+flip_angle = 9; % alpha - in degrees
 TE = 0.00419;
+ES = 0.0099; % echo spacing in seconds (this is because R1, PD and R2_star are in seconds in our data)
+TI = .960; % in seconds
+readout_duration = 176 * ES; % in seconds
+TR = 2.42;
+TD = TR - (TI + (readout_duration/2)); % delay time
 
-T1_star = (R1_image - ((1/echo_spacing) .* (log(cosd(flip_angle))))).^-1;
-E1 = exp(-TI .* R1_image);
-E2 = exp(-delay_time*R1_image);
+%% MPRAGE signal equation
+T1_star = (R1 - ((1/ES) .* (log(cosd(flip_angle))))).^-1;
+E1 = exp(-TI .* R1);
+E2 = exp(-TD*R1);
 E3 = exp(-(readout_duration ./  T1_star));
 E4 = exp(-(readout_duration ./ (2*T1_star)));
 
-t1 = (1 - 2.*E1 + E1.*E2) ./ (1 + E1.*E2.*E3);
-t2 = (1 + E1.*E2.*E3 - E4 - E4.*E1.*E2) ./ (1 + E1.*E2.*E3);
-fR1 = E4 .* t1 + T1_star .* R1_image .* t2; % this bit needs more thourough checking
+x1 = (1 - 2.*E1 + E1.*E2) ./ (1 + E1.*E2.*E3);
+x2 = (1 + E1.*E2.*E3 - E4 - E4.*E1.*E2) ./ (1 + E1.*E2.*E3);
+
 fPD = PD;
 fR2_star = exp(-TE * R2_star);
+fR1 = E4 .* x1 + T1_star .* R1 .* x2; 
 
-% T1w_image = real(fR1 .* sind(flip_angle)); % this is here just in case someone wants to get an MPRAGE from only R1 data
+% T1w_image = fR1 .* sind(flip_angle); % this is here just in case someone wants to get an MPRAGE from only R1 data
+% T1w_image = fR1 .* fPD .* sind(flip_angle); 
 T1w_image = fR1 .* fPD .* fR2_star .* sind(flip_angle); 
 
-T1w_image = T1w_image * 1000; % to convert to milliseconds
+%% Convert T1w image to milliseconds
+T1w_image = T1w_image * 1000;
 
+%% Set up descriptors
 T1w_header = header;
 T1w_header.fname = 'synthetic_mprage.nii';
-T1w_header.descrip = 'synthetic_mprage'; % perhaps should put more data in the descriptor eg: the parameters used for the generation of the mprage
+T1w_header.descrip = ['synthetic_mprage ' , ...
+    'TE=' , num2str(TE*1000) , 'ms ' , ...
+    'TR=' , num2str(TR*1000) , 'ms ' , ...
+    'ES=' , num2str(ES*1000) , 'ms ' , ...
+    'TD=' , num2str(TD*1000) , 'ms ' , ...
+    'TI=' , num2str(TI*1000) , 'ms ' , ...
+    'Readout Duration (theta)=' , num2str(readout_duration*1000) , 'ms'];
